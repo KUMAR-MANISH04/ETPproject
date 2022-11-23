bca8ec46636880020eed5f3473b356d70c96f36d
KUMAR-MANISH04 <7050maneesh@gmail.com>
2022-11-23 08:59:54 +0530

    ETP project of dear diary

---
+++ Dear Diary/app/src/main/java/com/gtappdevelopers/DearDiary/NotesDao.kt
@@ -0,0 +1,22 @@
+package com.gtappdevelopers.DearDiary
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NotesDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note :Note)
+
+    @Update
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("Select * from notesTable order by id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+
+}
